3efb887f22ef6e17e478143c8bc349155933eca1
yuri.freire <yuri_4017@hotmail.com>
2023-05-07 11:56:03 -0300

    Criando estrutura do projeto

---
+++ BookStore.BookStoreApi/BookStore.BookStoreApi.Api/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace BookStore.BookStoreApi.Api
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ BookStore.BookStoreApi/BookStore.BookStoreApi.Domain/Entities/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.BookStoreApi.Domain.Entities
+{
+    public sealed class Person
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Document { get; private set; }
+        public string Phone { get; private set; }
+    }
+
+    public Person(string document, string name, string phone)
+    {
+        return new Person();
+    }
+}
