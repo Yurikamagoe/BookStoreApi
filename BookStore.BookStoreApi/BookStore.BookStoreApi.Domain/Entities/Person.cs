36deeffaca331649379022fc98ba9373c2283851
yuri.freire <yuri_4017@hotmail.com>
2023-05-07 17:08:04 -0300

    Criando classe Person e adicionando contrutores e validações sobre os atributos

--- BookStore.BookStoreApi/BookStore.BookStoreApi.Domain/Entities/Person.cs
+++ BookStore.BookStoreApi/BookStore.BookStoreApi.Domain/Entities/Person.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookStore.BookStoreApi.Domain.Validations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,10 +13,28 @@ namespace BookStore.BookStoreApi.Domain.Entities
         public string Name { get; private set; }
         public string Document { get; private set; }
         public string Phone { get; private set; }
-    }
 
-    public Person(string document, string name, string phone)
-    {
-        return new Person();
+        public Person(string document, string name, string phone)
+        {
+            Validation(document, name, phone)
+        }
+
+        public Person(int id, string document, string phone)
+        {
+            DomainValidationException.When(id < 0, "Id deve ser conter um valor válido");
+            Id = id;
+        }
+
+        private void Validation(string document, string name, string phone)
+        {
+            DomainValidationException.When(string.IsNullOrEmpty(document), "Documento deve ser informado");
+            DomainValidationException.When(string.IsNullOrEmpty(name), "Nome deve ser informado");
+            DomainValidationException.When(string.IsNullOrEmpty(phone), "Telefone deve ser informado");
+
+            Name = name;
+            Document = document;
+            Phone = phone;
+        }
+
     }
 }
